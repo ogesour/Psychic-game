308eacb8004b2b59ee34ad9f10784a4fde510592
ogesour <ogesour@gmail.com>
2018-10-14 14:24:42 -0700

    update

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,1 +1,8 @@
-alert("hi Eric");
+var letterSet = ["a", "b", "c","d","e","f","g","h","i","j","k","l","m","o","p","q","r","s","t","u","v","w","x","y","z"];
+
+var win = 0;
+var loss = 0;
+var guess = 9;
+var usedGues = [];
+
+console.log(win); 
