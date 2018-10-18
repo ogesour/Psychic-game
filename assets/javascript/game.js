e0f8454d8a7435cb93bdb464cd3f542aba61e81d
ogesour <ogesour@gmail.com>
2018-10-17 20:40:01 -0700

    updates

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,8 +1,35 @@
-var letterSet = ["a", "b", "c","d","e","f","g","h","i","j","k","l","m","o","p","q","r","s","t","u","v","w","x","y","z"];
 
+// Set of Gloabl Variables.
+
+var letterSet = ["a", "b", "c","d","e","f","g","h","i","j","k","l","m","o","p","q","r","s","t","u","v","w","x","y","z"];
 var win = 0;
 var loss = 0;
 var guess = 9;
 var usedGues = [];
 
-console.log(win); 
+// Code to select random letter. 
+    var randomLetter = letterSet[Math.floor(Math.random() * letterSet.length)];
+    console.log(randomLetter);
+
+    function newLetter(){
+        randomLetter = letterSet[Math.floor(Math() * letterSet.length)]
+    };
+
+    document.onkeyup = function(event){
+        var guess = event.key;
+
+        if(guess === randomLetter){
+            win++;
+        }else{
+            guess--
+        }
+        if(guess = 0){
+            loss++
+        }
+    
+
+    document.getElementById("wins").innerHTML = win;
+    document.getElementById("losses").innerHTML = loss;
+    document.getElementById("guessLeft").innerHTML = guess;
+    // document.getElementById("currentGuess").innerHTML = usedGuess;
+    };
