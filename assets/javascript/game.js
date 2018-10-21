cb397c98239bfae2d3058c3e101d4ec1c4428a96
ogesour <ogesour@gmail.com>
2018-10-20 17:44:19 -0700

    revised countdown need to figure a reset

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,38 +1,95 @@
 
-// Set of Gloabl Variables.
+// Set of Gloabl Variables.----------
 
 var letterSet = ["a", "b", "c","d","e","f","g","h","i","j","k","l","m","o","p","q","r","s","t","u","v","w","x","y","z"];
 var win = 0;
 var loss = 0;
-var guess = 9;
+var guessLeft = 10;
 var usedGues = [];
-// Code to select random letter. 
+var letterToGuess;
+
+
+//set variable to select random letter from global variable
+var randomLetter = letterSet[Math.floor(Math.random() * letterSet.length)];
+//test
+console.log(randomLetter);
+
+
+
+if(guessLeft === 0){
+    resetGame();
+
+}; 
+
+
+//--------functions-----------------
+function printResult(){
+
+    document.getElementById('wins').innerHTML = win;
+    document.getElementById('losses').innerHTML = loss;
+    document.getElementById('guessLeft').innerHTML = guessLeft
+};
+
+
+
+function resetGame(){
+    var letterSet = ["a", "b", "c","d","e","f","g","h","i","j","k","l","m","o","p","q","r","s","t","u","v","w","x","y","z"];
+    var win = 0;
+    var loss = 0;
+    var guessLeft = 9;
+    var usedGues = [];
+    var letterToGuess;
     var randomLetter = letterSet[Math.floor(Math.random() * letterSet.length)];
-    console.log(randomLetter);
+}
 
 
-// New letter after win or loss
-    function newLetter(){
-        randomLetter = letterSet[Math.floor(Math() * letterSet.length)]
+document.onkeyup = function(event){
+
+    var userguess = event.key;
+
+    if ( userguess === randomLetter){
+        win++;
+        alert('you won')
+    }else{
+        guessLeft--;
     };
 
 
+    if(guessLeft === 0){
+        loss++;
+        alert('you lost');
+        
 
-    document.onkeyup = function(event){
-        var guess = event.key;
+    }
 
-        if(guess === randomLetter){
-            win++;
-        }else{
-            guess - 1;
-        };
-        if(guess === 0){
-            loss++;
-        };
+    printResult();
     
 
-    document.getElementById("wins").innerHTML = win;
-    document.getElementById("losses").innerHTML = loss;
-    document.getElementById("guessLeft").innerHTML = guess;
-    // document.getElementById("currentGuess").innerHTML = usedGuess;
-    };
+};
+
+
+
+//---calls-------------------------
+// printResult();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
+
+   
